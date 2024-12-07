9e44ced5219e2575b63248494b63d8bb58475774
Y2kun <me@y2kun.dev>
2024-12-07 16:03:14 +0100

    Finally finished day 03

--- 2024/03-Mull-It-Over/main.rb
+++ 2024/03-Mull-It-Over/main.rb
@@ -1,27 +1,40 @@
-# file_path = "input.txt"
-file_path = "test.txt"
+require 'strscan'
+
+file_path = "input.txt"
+# file_path = "test.txt"
 
 input = File.read(file_path)
 
 def part1(input)
-  matches = input.scan(/mul\((\d+),\s*(\d+)\)/)
+  matches = input.scan(/mul\((\d*),*(\d*)\)/)
   result = 0
   matches.map { |a, b| result += a.to_i * b.to_i}
   puts "Result P1: #{result}"
 end
 
 def part2(input)
+  strscanner = StringScanner.new(input)
   result = 0
-  matches = input.scan(/(do|don't)?\s*mul\((\d+),\s*(\d+)\)/)
-  matches.each do | match |
-    puts match.inspect
+  doing = true
+
+  while !strscanner.eos?
+    if strscanner.scan(/do\(\)/)
+      doing = true
+    elsif strscanner.scan(/don't\(\)/)
+      doing = false
+    elsif (match = strscanner.scan(/mul\((\d+),(\d+)\)/))
+      if doing
+        a, b = match.match(/mul\((\d+),(\d+)\)/).captures.map(&:to_i)
+        result += a * b
+      end
+    else
+      strscanner.getch
+    end
   end
- 
-  puts "Result P2: #{result}"
+
+    puts "Result P2: #{result}"
 end
 
-# part1(input)
+part1(input)
 part2(input)
 
-
-# make the regex incluede the do and don't
